c0ed1d4bb65469829420a3e5e7640cc4f3e6cf8b
foxnacity <foxnacity@gmail.com>
2021-12-11 17:48:20 +0100

    adding gatsby confi

--- src/components/loadingscreen.js
+++ src/components/loadingscreen.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import anim from '../images/animation.svg'
 import "../styles/loading/loading.scss"
 
 
 
 const LoadingPage = () =>{
+
+	function loadpage(){
+		const loadScreen = document.querySelector(".loading")
+		setTimeout(()=>{
+			loadScreen.remove()
+			document.body.style.overflowY = "scroll"
+		}, 5000)
+		
+	}
+
+	useEffect(()=>{
+		window.addEventListener("load", loadpage())
+		return () => window.removeEventListener("load", loadpage())
+	},[])
+
 	return(
 		<div className="loading">
 			<div className="loading__spinner">
