f538fe4672078a66c7ff471acee45bce04c28fd5
foxnacity <foxnacity@gmail.com>
2021-12-25 23:42:18 +0100

    completed about section

--- src/components/aboutSection.js
+++ src/components/aboutSection.js
@@ -1,28 +1,43 @@
-import React from 'react'
-import me from '../images/me.png'
+import React, { useEffect } from 'react'
+import "../styles/about/about.scss"
+import { StaticImage} from "gatsby-plugin-image"
 
 const AboutSection = () =>{
+    
+    useEffect(()=>{
+        const observer = new IntersectionObserver(entries =>{
+            entries.forEach(entry=>{
+             entry.target.classList.toggle("show", entry.isIntersecting)
+            })
+         },{
+             thresholds: 0.3,
+         })
+         if(typeof window !== undefined){
+             const section = document.querySelector("#about")
+             if(section){
+                 observer.observe(section)
+             }
+         }
+    },[])
+    
     return(
-        <section className="about">
-            <header className="about__header">
-                <div className="about__header--wrapper">
-                <h2>Damian "<span>Enodd</span>" Nowak</h2>
-                <p>Developer <span>/</span> Designer <span>/</span> Geek <span>/</span> Mountain Guys</p>
-                </div>
-                <div className="about__header--image">
-                    {
-                        /*
-                            TODO Gatsby image optimization, to include it into profile pic
-                        */
-                    }
-                    <img src={me} alt="me" />
-                </div>
-            </header>
-            <main className="about__main">
-                <p>
-                    My name is Damian, I'm also known as Enodd and I'm selftaught <span>Front-end Developer</span>, that's trying his best in this industry
-                </p>
-            </main>
+        <section className="about" id="about">
+            <div className="about__wrapper">
+                <header className="about__header">
+                    <div className="about__header--wrapper">
+                    <h2>Damian "<span>Enodd</span>" Nowak</h2>
+                    <p>Developer <span>/</span> Designer <span>/</span> Geek</p>
+                    </div>
+                    <div className="about__header--image">
+                        <StaticImage src='../images/me.png' alt="me" />
+                    </div>
+                </header>
+                <main className="about__main">
+                    <p>
+                        My name is Damian, I'm also known as Enodd <br /> and I'm selftaught <span>Front-end Developer</span>, that's trying his best in this industry
+                    </p>
+                </main>    
+            </div>
         </section>
     )
 }
