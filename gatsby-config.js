57d9adf12734d3f92154b5c607f6fa3277ac468c
Enodd <foxnacity@gmail.com>
2022-03-28 12:16:14 +0200

    Added configuration files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,67 @@
 /** @type {import('gatsby').GatsbyConfig} */
+
+const path = require("path");
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        "@components": path.resolve(__dirname, "src/components"),
+        "@data": path.resolve(__dirname, "src/data"),
+        "@images": path.resolve(__dirname, "src/images"),
+        "@views": path.resolve(__dirname, "src/views"),
+        "@pages": path.resolve(__dirname, "src/pages"),
+      },
+    },
+  });
+};
+
 module.exports = {
   siteMetadata: {
-      title: ``,
-    siteUrl: `https://www.yourdomain.tld`
+    title: ``,
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-transformer-remark", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "enodd.github.io",
+        description: "Personal Portfolio Website",
+        lang: "en",
+        display: "standalone",
+        icon: `./src/images/icons/favicon.png`,
+        icons: [
+          {
+            src: `./src/images/icons/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `./src/images/icons/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+        start_url: "/",
+      },
     },
-    __key: "pages"
-  }]
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/pages/",
+      },
+      __key: "images",
+    },
+  ],
 };
