bd239f3217b6d0ceb1bceb798b42353ae3969426
Enodd <foxnacity@gmail.com>
2024-01-09 10:23:51 +0100

    added tailwind and nextui

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { NextUIProvider } from '@nextui-org/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <NextUIProvider>
+      <div className='dark'>
+        <App />
+      </div>
+    </NextUIProvider>
   </React.StrictMode>,
 )
