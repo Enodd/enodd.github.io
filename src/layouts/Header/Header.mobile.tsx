3269726b9f84ce4afede4e63053627a24628e7ee
Enodd <foxnacity@gmail.com>
2024-09-24 19:32:30 +0200

    feat: working mobile header animation

--- src/layouts/Header/Header.mobile.tsx
+++ src/layouts/Header/Header.mobile.tsx
@@ -1,5 +1,5 @@
 import gsap from 'gsap';
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 const openState = {
     opacity: 1,
@@ -9,32 +9,30 @@ const openState = {
 export const HeaderMobile: React.FC = () => {
     const [isVisible, setIsVisible] = useState<boolean>(false);
     const menuRef = useRef<HTMLDivElement>(null);
+    const container = useRef<HTMLElement>(null);
 
     const handleToggle = () => {
-        const timeline = gsap.timeline({
-            paused: true,
-            reversed: !isVisible,
-            onReverseComplete: () => {
-                console.log('reversed')
-                gsap.set(menuRef.current, { display: 'none' });
-            }
-        })
-        .set(menuRef.current, { display: 'flex' })
-        .fromTo(menuRef.current, { opacity: 0, scaleY: 0 }, openState)
-        .duration(0.3);
+        const timeline = gsap
+            .timeline({
+                paused: true,
+                reversed: !isVisible,
+                onReverseComplete() {
+                    console.log('completed')
+                    gsap.set(menuRef.current, {display: 'none'})
+                }
+            })
+            .set(menuRef.current, {display: 'flex'})
+            .fromTo(menuRef.current, {
+                opacity: 0,
+                scaleY: 0
+            }, openState)
+            .duration(0.3)
+        // REMEMBER TO PUT 0 INSIDE .reverse()
+        timeline.reversed() ? timeline.play() : timeline.reverse(0);
+        setIsVisible(prev => !prev)
+    }
 
-        console.log(isVisible, timeline.reversed());
-        if (timeline.reversed()) {
-            console.log('open');
-            timeline.play();
-        } else {
-            console.log('close');
-            timeline.reverse();
-        }
-        setIsVisible(prev => !prev);
-    };
-
-    return <nav>
+    return <nav ref={container}>
         <div ref={menuRef} className="absolute top-0 left-0 h-screen w-screen bg-background-site flex flex-col items-center gap-3 py-4 px-8 z-10 origin-top" style={{ display: 'none' }}>
             <button className="self-end" onClick={handleToggle}>
                 close
