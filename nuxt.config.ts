de00abcfe5670037d517a414fd3cd982fcb662f1
Damian Nowak <Foxnacity@gmail.com>
2026-03-10 23:12:46 +0100

    feat: initial commit for nuxt project

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxt/content', '@nuxt/eslint'],
+  devtools: { enabled: true },
+  compatibilityDate: '2024-04-03',
+})
